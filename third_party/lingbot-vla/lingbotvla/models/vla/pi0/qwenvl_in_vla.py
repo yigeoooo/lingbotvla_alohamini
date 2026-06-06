301a30b22defbb720e977ad9fe8df9888a20fde0
yigeoooo <632084210@qq.com>
2026-06-06 11:00:42 +0800

    fix： transformer计算策略修改

--- third_party/lingbot-vla/lingbotvla/models/vla/pi0/qwenvl_in_vla.py
+++ third_party/lingbot-vla/lingbotvla/models/vla/pi0/qwenvl_in_vla.py
@@ -1322,7 +1322,7 @@ class Qwen2_5_VLForConditionalGeneration(Qwen2_5_VLPreTrainedModel, GenerationMi
 
     def __init__(self, config, **kwargs):
         super().__init__(config)
-        self.visual = Qwen2_5_VisionTransformerPretrainedModel._from_config(config.vision_config, use_flash_attention_2=True)
+        self.visual = Qwen2_5_VisionTransformerPretrainedModel._from_config(config.vision_config)
         self.model = Qwen2_5_VLModel(config)
         self.vocab_size = config.vocab_size
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
