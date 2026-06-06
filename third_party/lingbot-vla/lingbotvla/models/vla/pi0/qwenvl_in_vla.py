1684ad4a99f42c27bfe90b07aec03fd7ee396ce4
yigeoooo <632084210@qq.com>
2026-06-06 11:18:55 +0800

    fix: 修复函数实现

--- third_party/lingbot-vla/lingbotvla/models/vla/pi0/qwenvl_in_vla.py
+++ third_party/lingbot-vla/lingbotvla/models/vla/pi0/qwenvl_in_vla.py
@@ -41,6 +41,13 @@ logger = logging.get_logger(__name__)
 _CONFIG_FOR_DOC = "Qwen2_5_VLConfig"
 
 
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+
 class Qwen2_5_VLMLP(nn.Module):
     def __init__(self, config, bias: bool = False):
         super().__init__()
